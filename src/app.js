d1f7f4b0d2b23a3243b3b528f8d1c3bab8bcd142
ErickSalazar44 <erickearl22@gmail.com>
2023-07-18 21:51:42 -0500

    Portfolio desde el servidor

--- src/app.js
+++ src/app.js
@@ -1,18 +1,45 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-require('dotenv').config();
-const router = require('./routes');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+const router = require("./routes");
+const handlebars = require("express-handlebars"); // handlebars config
 
 const app = express();
 
-// Middlewares 
+// Middlewares
 app.use(express.json());
-app.use(helmet({
-    crossOriginResourcePolicy: false,
-}));
+app.use(
+    helmet({
+        crossOriginResourcePolicy: false,
+    })
+);
 app.use(cors());
 
-app.use('/', router);
+app.use(express.static("src"));
+
+app.use(express.static('public'));
+
+app.use(
+    helmet.contentSecurityPolicy({
+        useDefaults: true,
+        directives: {
+            "img-src": ["'self'", "https: data:"],
+        },
+    })
+);
+
+// handlebars config
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+app.engine(
+    "handlebars",
+    handlebars.engine({
+        layoutsDir: __dirname + "/views/layouts",
+        defaultLayout: "index",
+    })
+);
+
+app.use("/", router);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const projectsData = require('../projectsData');
 
 router.get('/', (req, res) => {
-    res.send("Welcome to express");
+    res.render("home");
 });
+
+router.get('/about-me', (req, res) => {
+    return res.render("aboutme")
+})
+
+
+router.get('/projects', (req, res) => {
+    return res.render("projects", {projectsData})
+})
+
+// para acceder al id 
+router.get('/projects/:id', (req, res) => {
+    const { id } = req.params
+    const projects = projectsData.find( project => project.id == id)
+    return res.render("projects-id", { projects })
+})
+
 
 
 module.exports = router;
